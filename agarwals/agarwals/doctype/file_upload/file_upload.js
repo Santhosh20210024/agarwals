623ea4a1c55bb3dad418ea5fc61d9e48b71a93df
Santhosh20210024 <svallatharasu@techfinite.com>
2023-12-05 16:25:11 +0530

    File Upload Completed

--- agarwals/agarwals/doctype/file_upload/file_upload.js
+++ agarwals/agarwals/doctype/file_upload/file_upload.js
@@ -7,23 +7,26 @@ frappe.ui.form.on('File Upload', {
     }
 });
 
+frappe.ui.form.on("File Upload", "refresh", function(frm) {
+  frm.set_df_property( "payer_type", "read_only", frm.is_new() ? 0 : 1);
+  frm.set_df_property( "bank_account", "read_only", frm.is_new() ? 0 : 1);
+})
 
-// Need to refactor later
 var set_css = function (frm){
     //debtor_report_upload
-    document.querySelectorAll("[data-fieldname='debtor_report_upload']")[1].style.backgroundColor ="black";
+    document.querySelectorAll("[data-fieldname='debtor_report_upload']")[1].style.backgroundColor ="#2490EF";
     document.querySelectorAll("[data-fieldname='debtor_report_upload']")[1].style.color ="white";  
 
     //claimbook_upload
-    document.querySelectorAll("[data-fieldname='claimbook_upload']")[1].style.backgroundColor ="black";
+    document.querySelectorAll("[data-fieldname='claimbook_upload']")[1].style.backgroundColor ="#2490EF";
     document.querySelectorAll("[data-fieldname='claimbook_upload']")[1].style.color ="white";  
 
     //settlement_advice_upload
-    document.querySelectorAll("[data-fieldname='settlement_advice_upload']")[1].style.backgroundColor ="black";
+    document.querySelectorAll("[data-fieldname='settlement_advice_upload']")[1].style.backgroundColor ="#2490EF";
     document.querySelectorAll("[data-fieldname='settlement_advice_upload']")[1].style.color ="white";  
 
     //bank_statement_upload
-    document.querySelectorAll("[data-fieldname='bank_statement_upload']")[1].style.backgroundColor ="black";
-    document.querySelectorAll("[data-fieldname='bank_statement_upload']")[1].style.color ="white";  
+    document.querySelectorAll("[data-fieldname='bank_statement_upload']")[1].style.backgroundColor ="#2490EF";
+    document.querySelectorAll("[data-fieldname='bank_statement_upload']")[1].style.color ="white";
 
 }
