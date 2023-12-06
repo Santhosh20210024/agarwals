50a5c61cec52165e83747e59960fc0a8312f8b1b
Santhosh20210024 <svallatharasu@techfinite.com>
2023-12-06 10:30:36 +0530

    settlement doctype creation

---
+++ agarwals/settlement/doctype/settlement_advice/test_settlement_advice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Agarwals and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSettlementAdvice(FrappeTestCase):
+	pass
