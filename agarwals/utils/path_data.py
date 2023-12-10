1f3a39b48960b0330effece29e2d37a3a752fb81
Santhoshsivan_tfs <139832209+Santhosh20210024@users.noreply.github.com>
2023-12-11 02:35:48 +0530

    Update path_data.py

--- agarwals/utils/path_data.py
+++ agarwals/utils/path_data.py
@@ -3,7 +3,7 @@ import frappe
 PROJECT_FOLDER = "DrAgarwals"
 HOME_PATH = "Home/DrAgarwals"
 SHELL_PATH = "private/files"
-SUB_DIR = ["Extract", "Tranform", "Load"]
+SUB_DIR = ["Extract", "Transform", "Load"]
 INNER_SUB_DIR = ["Error"]
 SITE_PATH = os.getcwd() + frappe.get_site_path()[1:]
 
