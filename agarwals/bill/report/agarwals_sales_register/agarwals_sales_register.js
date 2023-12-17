5420df185bfef6d2c6e069c2ffe5d0c91c652dde
Santhosh20210024 <svallatharasu@techfinite.com>
2023-12-17 15:54:09 +0530

    changes on agarwals_sales_register

---
+++ agarwals/bill/report/agarwals_sales_register/agarwals_sales_register.js
@@ -0,0 +1,44 @@
+frappe.query_reports["Agarwals Sales Register"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname":"customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"mode_of_payment",
+			"label": __("Mode of Payment"),
+			"fieldtype": "Link",
+			"options": "Mode of Payment"
+		},
+		{
+			"fieldname":"cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "Link",
+			"options": "Cost Center"
+		}
+	]
+}
+
+erpnext.utils.add_dimensions('Dr. Agarwals Sales Register', 7);
