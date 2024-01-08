77933e5a29cc306be85a485bc64dfd31afd20c4d
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-01-08 18:59:41 +0530

    removed excel parser

--- agarwals/agarwals/doctype/excel_parser/excel_parser.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, Agarwals and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ExcelParser(Document):
-	pass
--- agarwals/agarwals/doctype/excel_parser/test_excel_parser.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Agarwals and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestExcelParser(FrappeTestCase):
-	pass
