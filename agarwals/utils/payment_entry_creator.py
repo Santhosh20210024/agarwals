2b19f8419e3b0e5d57020e1dc2913c82deb971b4
Balamurugan1207 <bramachandran@techfinite.com>
2024-02-07 10:15:54 +0530

    Debugged Error

--- agarwals/utils/payment_entry_creator.py
+++ agarwals/utils/payment_entry_creator.py
@@ -16,14 +16,25 @@ class PaymentEntryCreator:
     def trim_and_remove_leading_zeros(self, text):
         return str(text).strip().lstrip('0')
 
+
+    def log_error(self, doctype, record_name = None, error_msg = None):
+        error_log_record = frappe.new_doc('Payment Entry Error Log')
+        error_log_record.set('reference_doctype',doctype)
+        if record_name:
+            error_log_record.set('reference_record',record_name)
+
+        error_log_record.set('error_message',error_msg)
+        error_log_record.save()
+        frappe.db.commit()
+
     def get_document_record(self, doctype, name):
         return frappe.get_doc(doctype,name)
 
     def get_matched_sa_records(self, reference_number):
         matched_records = []
         for settlement_advice_record in self.settlement_advice_records:
-            utr_number = self.strip_leading_zeros(settlement_advice_record['utr_number'])
-            final_utr_number = self.strip_leading_zeros(settlement_advice_record['final_utr_number'])
+            utr_number = self.trim_and_remove_leading_zeros(settlement_advice_record['utr_number'])
+            final_utr_number = self.trim_and_remove_leading_zeros(settlement_advice_record['final_utr_number'])
             if reference_number != utr_number or reference_number != final_utr_number:
                 continue
             elif reference_number == utr_number:
@@ -101,7 +112,7 @@ class PaymentEntryCreator:
                     match_log.append({
                         'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > cb_{match_with}[{record[match_with]}] > cb_bill_no[{record['custom_raw_bill_number']}] > dbr_bill_no[{bill_record['name']}] > dbr_claim[{bill_record['claim_id']}] > Matched",
                         'status': 'Success', 'order': order + 1})
-            return matched_bills,matched_claims,match_log
+        return matched_bills,matched_claims,match_log
 
     def match_with_bill(self,sa_record,bank_utr,match_log,order):
         matched_bills = []
@@ -115,23 +126,25 @@ class PaymentEntryCreator:
             if matched_bill_records:
                 matched_bills.append(record)
                 match_log.append({
-                    'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > dbr_claim[{bill_record['claim_id']}]",
+                    'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > dbr_claim[{record['claim_id']}]",
                     'status': 'Success', 'order': order + 1})
         return matched_bills, match_log
 
     def get_matched_bill_and_claim(self,sa_record,bank_utr):
         match_log = []
+        matched_bill = []
+        matched_claim = []
         # If Bill No in Settlement Advice trying to match with Debtors Bill No
         if sa_record.bill_no:
             bill_record = self.get_bill_record_with_bill_no(sa_record.bill_no)
-            if not bill_record:
+            if bill_record:
                 match_log.append({
-                                     'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_bill[{sa_record.bill_no}] > dbr_bill[no bill record]",
-                                     'status': 'Fail', 'order': 1})
+                    'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_bill[{sa_record.bill_no}] > dbr_bill[{bill_record['name']}]",
+                    'status': 'Success', 'order': 1})
+                return bill_record, None, match_log
             match_log.append({
-                                 'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_bill[{sa_record.bill_no}] > dbr_bill[{bill_record['name']}]",
-                                 'status': 'Success', 'order': 1})
-            return bill_record, None, match_log
+                                 'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_bill[{sa_record.bill_no}] > dbr_bill[no bill record]",
+                                 'status': 'Fail', 'order': 1})
         match_log.append({
             'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_bill[no bill number]",
             'status': 'Fail', 'order': 1})
@@ -141,6 +154,9 @@ class PaymentEntryCreator:
                 return matched_bill[0], matched_claim[0], match_log
             sa_record.set('remark',str(sa_record.remark) + '\nMore than 1 bill Matched')
             sa_record.save()
+        match_log.append({
+            'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > cb_al[No claim Record Found]",
+            'status': 'Fail', 'order': 2})
         matched_bill, matched_claim, match_log = self.match_with_claimbook_and_bill(sa_record, bank_utr, match_log,
                                                                                     'cl_number', 2)
         if matched_bill:
@@ -148,15 +164,21 @@ class PaymentEntryCreator:
                 return matched_bill[0], matched_claim[0], match_log
             sa_record.set('remark', str(sa_record.remark) + '\nMore than 1 bill Matched')
             sa_record.save()
+        match_log.append({
+            'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > cb_cl[No claim Record Found]",
+            'status': 'Fail', 'order': 3})
         matched_bill, match_log = self.match_with_bill(sa_record, bank_utr, match_log, 3)
         if matched_bill:
             if len(matched_bill) == 1:
                 return matched_bill[0], None, match_log
             sa_record.set('remark', str(sa_record.remark) + '\nMore than 1 bill Matched')
             sa_record.save()
-        return None
+        match_log.append({
+            'log': f"bank_utr[{bank_utr}] > sa_utr[{sa_record.utr_number}] > sa_claim[{sa_record.claim_id}] > dbr_claim[No Bill Record Found]",
+            'status': 'Fail', 'order': 4})
+        return None, None, match_log
 
-    def create_payment_entry_record_and_update_bank_transaction(self, bank_transaction, sales_invoice, bank_account, settled_amount, tds_amount = 0, disallowed_amount = 0):
+    def create_payment_entry_and_update_bank_transaction(self, bank_transaction, sales_invoice, bank_account, settled_amount, tds_amount = 0, disallowed_amount = 0):
         try:
             deductions = []
             payment_entry_record = frappe.new_doc('Payment Entry')
@@ -219,8 +241,17 @@ class PaymentEntryCreator:
         settlement_advice_record.save()
         frappe.db.commit()
 
+    def get_company_account(self, bank_account_name):
+        bank_account = frappe.get_doc('Bank Account', bank_account_name)
+        if not bank_account.account:
+            return None
+        return bank_account.account
+
     def process(self):
+        count = 1
+        print("___________________________________________",count)
         for bank_transaction_record in self.bank_transaction_records:
+            count = count + 1
             if not bank_transaction_record['date']:  #If Date is null skip to next record
                 self.log_error('Bank Transaction', bank_transaction_record['name'], "Date is Null")
                 continue
@@ -243,8 +274,9 @@ class PaymentEntryCreator:
                                                        key=lambda x: (x['tds_amount'], x['disallowed_amount']),
                                                        reverse=True) #Sort the Settlement Advice with TDS Amount and then Disallowed Amount
             for record in matched_sa_records:
+                bank_amount = 0
                 settlement_advice = self.get_document_record('Settlement Advice',record['name'])
-                if settlement_advice.settled_amount == settlement_advice_record.claim_amount:
+                if settlement_advice.settled_amount == settlement_advice.claim_amount:
                     settled_amount = settlement_advice.settled_amount - settlement_advice.tds_amount - settlement_advice.disallowed_amount
                     tds_amount = settlement_advice.tds_amount
                     disallowed_amount = abs(settlement_advice.disallowed_amount)
@@ -253,31 +285,35 @@ class PaymentEntryCreator:
                     tds_amount = settlement_advice.tds_amount
                     disallowed_amount = abs(settlement_advice.disallowed_amount)
                 bank_transaction = self.get_document_record('Bank Transaction',bank_transaction_record['name'])
+                if bank_transaction.unallocated_amount == 0:
+                    break
                 if settled_amount > bank_transaction.unallocated_amount:
-                    continue
-                matched_bill,matched_claim,match_log = self.get_matched_bill_and_claim(settlement_advice,bank_account.reference_number)
+                    settled_amount = bank_transaction.unallocated_amount
+                    bank_amount = bank_transaction.unallocated_amount
+                matched_bill,matched_claim,match_log = self.get_matched_bill_and_claim(settlement_advice,bank_transaction.reference_number)
                 if not matched_bill:
-                    self.add_match_log(match_logs, settlement_advice)
+                    self.add_match_log(match_log, settlement_advice)
                     settlement_advice.set('status', 'Error')
                     settlement_advice.set('matched_bank_transaction', bank_transaction_record.name)
                     settlement_advice.save()
                     frappe.db.commit()
                     continue
-                if not matched_claim:
-                    self.add_match_log(match_logs, settlement_advice)
+                sales_invoice = self.get_document_record('Sales Invoice', matched_bill['name'])
+                if matched_claim:
+                    self.add_match_log(match_log, settlement_advice)
+                    settlement_advice.set('matched_bank_transaction', bank_transaction.name)
+                    settlement_advice.set('matched_claimbook_record', matched_claim['name'])
+                    settlement_advice.set('matched_bill_record', matched_bill['name'])
+                    settlement_advice.save()
+                    frappe.db.set_value('Sales Invoice', sales_invoice.name, 'custom_insurance_name',
+                                        matched_claim['insurance_company_name'])
+                    frappe.db.commit()
+                else:
+                    self.add_match_log(match_log, settlement_advice)
                     settlement_advice.set('matched_bank_transaction', bank_transaction.name)
                     settlement_advice.set('matched_bill_record', matched_bill['name'])
                     settlement_advice.save()
                     frappe.db.commit()
-                self.add_match_log(match_logs, settlement_advice_record)
-                settlement_advice.set('matched_bank_transaction', bank_transaction.name)
-                settlement_advice.set('matched_claimbook_record', matched_claim['name'])
-                settlement_advice.set('matched_bill_record', matched_bill['name'])
-                settlement_advice.save()
-                sales_invoice = self.get_document_record('Sales Invoice',matched_bill['name'])
-                frappe.db.set_value('Sales Invoice', sales_invoice.name, 'custom_insurance_name',
-                                    matched_claim['insurance_company_name'])
-                frappe.db.commit()
                 if sales_invoice.outstanding_amount < settled_amount:
                     self.log_error('Settlement Advice', settlement_advice.name,
                                    "Settled amount is greater than Outstanding Amount")
@@ -290,7 +326,7 @@ class PaymentEntryCreator:
                         payment_entry_created = self.create_payment_entry_and_update_bank_transaction(
                             bank_transaction, sales_invoice, bank_account, settled_amount,
                             tds_amount)
-                        settlement_advice_record.set('remark', 'Disallowance amount is greater than Outstanding Amount')
+                        settlement_advice.set('remark', 'Disallowance amount is greater than Outstanding Amount')
                     else:
                         payment_entry_created = self.create_payment_entry_and_update_bank_transaction(
                             bank_transaction,
@@ -301,8 +337,8 @@ class PaymentEntryCreator:
                         settlement_advice.set('status', 'Partially Processed')
                     else:
                         settlement_advice.set('remark', 'Unable to Create Payment Entry')
-                        settlement_advice_record.set('status', 'Warning')
-                    settlement_advice_record.save()
+                        settlement_advice.set('status', 'Warning')
+                    settlement_advice.save()
                     frappe.db.commit()
                 else:
                     payment_entry_created = self.create_payment_entry_and_update_bank_transaction(
@@ -310,9 +346,12 @@ class PaymentEntryCreator:
                         sales_invoice, bank_account,
                         settled_amount, tds_amount, disallowed_amount)
                     if payment_entry_created:
-                        settlement_advice.set('status', 'Fully Processed')
+                        if bank_amount == 0:
+                            settlement_advice.set('status', 'Fully Processed')
+                        else:
+                            settlement_advice.set('status', 'Partially Processed')
                     else:
                         settlement_advice.set('remark','Unable to Create Payment Entry')
                         settlement_advice.set('status', 'Warning')
-                    settlement_advice_record.save()
+                    settlement_advice.save()
                     frappe.db.commit()
