9783be879fd94567f5e955d71fc09c1c6c8e64b8
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-02-24 12:42:48 +0530

    called the class inside the job using params

--- agarwals/website_downloader/downloader_job.py
+++ agarwals/website_downloader/downloader_job.py
@@ -1,5 +1,5 @@
 import frappe
-from agarwals.website_downloader.downloader import Mediassit
+from agarwals.website_downloader.downloader import Mediassist
 
 def enqueue_job(tpa_name,branch_name):
-    frappe.enqueue(Mediassit().download,queue='long', is_async=True, job_name="TPA_downloader", timeout=25000,tpa_name=tpa_name,branch_name=branch_name)
+    frappe.enqueue(tpa_name(branch_name).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=25000)
