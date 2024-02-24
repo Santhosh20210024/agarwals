73c626800b2385799b10b861754de95947a182e6
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-02-24 15:39:06 +0530

    updated the enqueue

--- agarwals/website_downloader/downloader_job.py
+++ agarwals/website_downloader/downloader_job.py
@@ -1,5 +1,7 @@
 import frappe
-from agarwals.website_downloader.downloader import Mediassist
+from agarwals.website_downloader import downloader
 
+@frappe.whitelist()
 def enqueue_job(tpa_name,hospital_branch):
-    frappe.enqueue(tpa_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=16000)
+    class_name=getattr(downloader,tpa_name)
+    frappe.enqueue(class_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=16000)
