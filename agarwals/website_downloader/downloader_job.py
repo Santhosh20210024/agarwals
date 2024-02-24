0d9ce2399103925fc05cacd17e922843878fa1e9
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-02-24 12:45:11 +0530

    change the timeout

--- agarwals/website_downloader/downloader_job.py
+++ agarwals/website_downloader/downloader_job.py
@@ -2,4 +2,4 @@ import frappe
 from agarwals.website_downloader.downloader import Mediassist
 
 def enqueue_job(tpa_name,hospital_branch):
-    frappe.enqueue(tpa_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=25000)
+    frappe.enqueue(tpa_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=16000)
