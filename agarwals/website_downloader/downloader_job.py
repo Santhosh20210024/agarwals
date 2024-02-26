aba644a2581ce793ceddeecab5b07cd0f677f14f
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-02-26 09:51:33 +0530

    change the time for the enque job

--- agarwals/website_downloader/downloader_job.py
+++ agarwals/website_downloader/downloader_job.py
@@ -4,4 +4,4 @@ from agarwals.website_downloader import downloader
 @frappe.whitelist()
 def enqueue_job(tpa_name,hospital_branch):
     class_name=getattr(downloader,tpa_name)
-    frappe.enqueue(class_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=16000)
+    frappe.enqueue(class_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=3600)
