e7ae9d8d26a46a585cd54307f9875b305366fdf5
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-02-27 14:53:11 +0530

    changed name of the method

--- agarwals/website_downloader/downloader_job.py
+++ agarwals/website_downloader/downloader_job.py
@@ -1,7 +1,8 @@
 import frappe
 from agarwals.website_downloader import downloader
 
-@frappe.whitelist()
-def enqueue_job(tpa_name,hospital_branch):
+@frappe.whitelist(allow_guest=True)
+def settlement_advice_job(tpa_name,hospital_branch):
+    tpa_name=tpa_name.replace(" ","_")
     class_name=getattr(downloader,tpa_name)
-    frappe.enqueue(class_name(hospital_branch).download,queue='long', is_async=True, job_name="TPA_downloader", timeout=3600)
+    frappe.enqueue(class_name(hospital_branch).download,queue='long', is_async=True, job_name=f"TPA_downloader_{str(tpa_name)}_{hospital_branch}", timeout=3600)
