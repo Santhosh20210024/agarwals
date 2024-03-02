8ad09834415792dae44b56481e0d4d27e05f460b
Santhoshsivan_tfs <139832209+Santhosh20210024@users.noreply.github.com>
2024-03-02 14:19:35 +0530

    Update claim_key_mapper.py

--- agarwals/utils/claim_key_mapper.py
+++ agarwals/utils/claim_key_mapper.py
@@ -90,7 +90,7 @@ class SAClaimKeyMapper(BillClaimKeyMapper):
 @frappe.whitelist()
 def map_claim_key():
     n = 1000
-    bill_records = frappe.get_list("Bill", filters={'claim_key': ''},
+    bill_records = frappe.get_list("Bill", filters={'claim_key': '', 'claim_id': ['not in', ['', '0']]},
                                        pluck='name')
     
     for i in range(0, len(bill_records), n):
