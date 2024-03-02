5e05a3e68ea2c27beb023e38bdd4bb8192fa60c7
Santhoshsivan_tfs <139832209+Santhosh20210024@users.noreply.github.com>
2024-03-02 16:05:06 +0530

    Update claim_key_mapper.py

--- agarwals/utils/claim_key_mapper.py
+++ agarwals/utils/claim_key_mapper.py
@@ -90,7 +90,7 @@ class SAClaimKeyMapper(BillClaimKeyMapper):
 @frappe.whitelist()
 def map_claim_key():
     n = 1000
-    bill_records = frappe.db.sql("""select name from tabBill where claim_key is NULL or ma_claim_key is NULL""")
+    bill_records = frappe.db.sql("""select name from tabBill where claim_key is NULL or ma_claim_key is NULL""", as_dict = True)
     bill_records = [i['name'] for i in bill_records]
 
     for i in range(0, len(bill_records), n):
