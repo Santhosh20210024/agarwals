73d6b62c1c952234756ce8f68714309168e3ff20
kavi-25 <kramalingam@techfinite.com>
2024-03-08 13:40:21 +0530

    safeway corrertion and health india downloader using selenium

--- agarwals/settlement_advice_downloader/safeway_downloader.py
+++ agarwals/settlement_advice_downloader/safeway_downloader.py
@@ -12,33 +12,42 @@ class SafewayDownloader(SeleniumDownloader):
         self.url = "https://www.safewaytpa.in/Hospitallogin.aspx"
 
     def login(self):
-        self.wait.until(EC.visibility_of_element_located(By.ID, "ctl00_ContentPlaceHolder1_txt_username"))
-        username = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txt_username")
-        username.send_keys(self.user_name)  # input username
-        password = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txt_pwd")
-        password.send_keys(self.password)  # input password
-        submit = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_ImageButton1").click()
+        try:
+            self.wait.until(EC.visibility_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_txt_username")))
+            username = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txt_username")
+            username.send_keys(self.user_name)  # input username
+            password = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txt_pwd")
+            password.send_keys(self.password)  # input password
+            submit = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_ImageButton1").click()
+        except Exception as e:
+            print(e)
 
     def navigate(self):
-        self.wait.until(EC.visibility_of_all_elements_located(By.LINK_TEXT, "Claim MIS"))
-        self.driver.find_element(By.LINK_TEXT, "Claim MIS").click()
-        from_date = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_DtpFrom_Date5")
-        from_date.send_keys('02/12/2023')  # input from date
-        to_date = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Dtpto_Date5")
-        to_date.send_keys('31/03/2024')  # input to date
-        self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Button1").click() #submit
+        try:
+            self.wait.until(EC.visibility_of_all_elements_located((By.LINK_TEXT, "Claim MIS")))
+            self.driver.find_element(By.LINK_TEXT, "Claim MIS").click()
+            from_date = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_DtpFrom_Date5")
+            from_date.send_keys('02/12/2023')  # input from date
+            to_date = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Dtpto_Date5")
+            to_date.send_keys('31/03/2024')  # input to date
+            self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Button1").click() #submit
+        except Exception as e:
+            print(e)
 
     def download(self):
-        self.wait.until(EC.visibility_of_element_located,(By.ID, "ctl00_ContentPlaceHolder1_lbltot"))
-        record_count = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_lbltot")
-        inner_html = record_count.get_attribute("innerHTML")
-        if inner_html == 'No Record Found':
-            return False
-        else:
-            self.wait.until(EC.visibility_of_element_located,(By.ID,"ctl00_ContentPlaceHolder1_btnExportInXLS"))
-            self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_btnExportInXLS").click() 
-            time.sleep(10)
-            return True
+        try:
+            self.wait.until(EC.visibility_of_element_located,((By.ID, "ctl00_ContentPlaceHolder1_lbltot")))
+            record_count = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_lbltot")
+            inner_html = record_count.get_attribute("innerHTML")
+            if inner_html == 'No Record Found':
+                return False
+            else:
+                self.wait.until(EC.visibility_of_element_located,(By.ID,"ctl00_ContentPlaceHolder1_btnExportInXLS"))
+                self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_btnExportInXLS").click() 
+                time.sleep(10)
+                return True
+        except Exception as e:
+            print(e)
 
 @frappe.whitelist()
 def initiator():
