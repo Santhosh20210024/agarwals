d950a49413086847ca0ee9b1b4499a9f01a5742a
Santhoshsivan_tfs <139832209+Santhosh20210024@users.noreply.github.com>
2024-03-12 10:10:56 +0000

    changes on main

--- agarwals/utils/payment_entry_creator.py
+++ agarwals/utils/payment_entry_creator.py
@@ -222,14 +222,7 @@ def run_payment_entry():
     match_logic = tuple(frappe.get_single('Control Panel').match_logic.split(','))
 
     # Need to change as X00
-    bank_transaction_records = frappe.db.sql(
-        f"SELECT name, bank_account, reference_number, date FROM `tabBank Transaction`
-          WHERE name in (select bank_transaction from `tabMatcher` where match_logic in {match_logic} and status is null )
-          AND status IN ('Pending','Unreconciled')
-          AND LENGTH(reference_number) > 4 AND deposit > 10 AND reference_number not like 'X0%'
-          ORDER BY unallocated_amount DESC",
-        as_dict=True
-        )
+    bank_transaction_records = frappe.db.sql(f"SELECT name, bank_account, reference_number, date FROM `tabBank Transaction` WHERE name in (select bank_transaction from `tabMatcher` where match_logic in {match_logic} and status is null ) AND status IN ('Pending','Unreconciled') AND LENGTH(reference_number) > 4 AND deposit > 10 AND reference_number not like 'X0%' ORDER BY unallocated_amount DESC", as_dict=True)
 
     for i in range(0, len(bank_transaction_records), n):
         batch_number = batch_number + 1
