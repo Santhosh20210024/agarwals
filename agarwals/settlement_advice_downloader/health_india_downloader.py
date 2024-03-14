87d42147b00e5470e354fcfe76277c52a985e3b4
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-03-14 16:12:18 +0530

    intial refactor for health

--- agarwals/settlement_advice_downloader/health_india_downloader.py
+++ agarwals/settlement_advice_downloader/health_india_downloader.py
@@ -6,48 +6,29 @@ from selenium.webdriver.common.by import By
 import time
 
 class HealthIndiaDownloader(SeleniumDownloader):
-    def __init__(self):
-        super().__init__()
-        self.portal ='Health India'
-        self.url = 'https://healthindiatpa.com/Logins/LoginProvider.aspx'
-        self.count = 0
-
+    def __init__(self,tpa_name,branch_code,last_executed_time):
+        self.tpa=tpa_name
+        self.branch_code=branch_code
+        self.last_executed_time=last_executed_time
+        SeleniumDownloader.__init__(self)
+        
     def login(self):
-        try:
-            self.wait.until(EC.visibility_of_element_located((By.ID,'ctl00_ContentPlaceHolder1_txtprovidercode')))
-            user_name = self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_txtprovidercode')
-            user_name.send_keys(self.user_name)
-            password = self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_txtproviderpassword')
-            password.send_keys(self.password)
-            self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_btnLogin').click()
-        except Exception as e:
-            print(e)
+        self.wait.until(EC.visibility_of_element_located((By.ID,'ctl00_ContentPlaceHolder1_txtprovidercode')))
+        user_name = self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_txtprovidercode')
+        user_name.send_keys(self.user_name)
+        password = self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_txtproviderpassword')
+        password.send_keys(self.password)
+        self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_btnLogin').click()
 
     def navigate(self):
-        try:
-            claim_count =self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_lblsettled').get_attribute("innerHTML")
-            self.count = claim_count
-            if  claim_count == 0:
-                return None  
-            else:
-                url = 'https://healthindiatpa.com/Provider/claimshome.aspx?type=claims_settled'
-                self.driver.get(url)
-        except Exception as e:
-            print(e)
-
+        claim_count =self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_lblsettled').get_attribute("innerHTML")
+        if  claim_count == 0:
+            self.raise_exception("Claim count should not be 0")
+        else:
+            url = 'https://healthindiatpa.com/Provider/claimshome.aspx?type=claims_settled'
+            self.driver.get(url)
 
     def download(self):
-        try:
-            if self.count ==0:
-                return False
-            else:
-                self.wait.until(EC.visibility_of_element_located,((By.ID,'ctl00_ContentPlaceHolder1_lnk_ExportToExcel')))
-                self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_lnk_ExportToExcel').click()
-                time.sleep(10)
-                return True
-        except Exception as e:
-            print (e)
-
-@frappe.whitelist()
-def initiator():
-    HealthIndiaDownloader().process()
+        self.wait.until(EC.visibility_of_element_located,((By.ID,'ctl00_ContentPlaceHolder1_lnk_ExportToExcel')))
+        self.driver.find_element(By.ID,'ctl00_ContentPlaceHolder1_lnk_ExportToExcel').click()
+        time.sleep(10)
