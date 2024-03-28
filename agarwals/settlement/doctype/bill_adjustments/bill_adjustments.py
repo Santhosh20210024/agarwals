d117760f452ccb16b0ff8ba4fe0c457e1e7db8bf
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-03-28 11:06:15 +0530

    added the title and the posting date is set as current date if empty

--- agarwals/settlement/doctype/bill_adjustments/bill_adjustments.py
+++ agarwals/settlement/doctype/bill_adjustments/bill_adjustments.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2024, Agarwals and contributors
 # For license information, please see license.txt
-
+import frappe.utils
 # import frappe
 from frappe.model.document import Document
 
 class BillAdjustments(Document):
-	pass
+	def before_save(self):
+		if not self.posting_date:
+			self.posting_date=frappe.utils.nowdate()
