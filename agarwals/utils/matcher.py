dab72a2cd4f021b01128a1eba35e0f0ce42f0e73
Balamurugan <139839683+Balamurugan1207@users.noreply.github.com>
2024-04-22 11:53:40 +0530

    Update matcher.py

--- agarwals/utils/matcher.py
+++ agarwals/utils/matcher.py
@@ -106,7 +106,7 @@ class Matcher:
 	        (sa.cg_utr_number = bt.custom_cg_utr_number
 		        or sa.cg_formatted_utr_number = bt.custom_cg_utr_number )
 	        and (sa.claim_key is not null and (cb.al_key = sa.claim_key
-		        or cb.cl_key = sa.claim_key)
+		        or cb.cl_key = sa.claim_key))
 	        and (((bi.claim_key = cb.al_key or bi.claim_key = cb.cl_key) or (bi.ma_claim_key = cb.al_key or bi.ma_claim_key = cb.cl_key)) and (bi.cg_formatted_bill_number = cb.cg_formatted_bill_number))
 	        and CONCAT(bi.name,'-',bt.name) not in (SELECT name FROM `tabMatcher`)
             and bi.status != 'Cancelled'
