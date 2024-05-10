7671ed8e353356437cd5794b73f09356aacd696f
Santhosh20210024 <svallatharasu@techfinite.com>
2024-05-10 21:25:09 +0530

    Zip File Extract and Process

--- agarwals/agarwals/doctype/file_upload/file_upload.js
+++ agarwals/agarwals/doctype/file_upload/file_upload.js
@@ -5,7 +5,6 @@ var set_css = function () {
   document.querySelectorAll("[data-action='delete_rows']")[0].style.display = "None";
    
 }
-
 // Read Only Validation
 const verify_read_only = (frm) => {
   var isOldDocument = frm.doc.__islocal ? 0 : 1;
@@ -16,92 +15,12 @@ const verify_read_only = (frm) => {
   frm.set_df_property("file", "read_only", isOldDocument);
   frm.set_df_property("file_format", "read_only", isOldDocument);
 }
-
-
-function extract_zip(frm) {
-  if (frm.doc.status == 'Zip') {
-    frm.add_custom_button(__('Extract Zip'), function (frm) {
-      if(cur_frm.doc.document_type == 'Claim Book' || cur_frm.doc.document_type == 'Debtors Report'){
-        frappe.call({
-          method: 'agarwals.agarwals.doctype.file_upload.file_upload.run_extractor',
-          args: {
-            fid: cur_frm.docname,
-            ffield: null
-          },
-          freeze: true,
-          freeze_message: "Files are Extracting....",
-          callback: function (r) {
-            if (r.message) {
-              frappe.msgprint(r.message)
-            }
-          }
-        });
-      }
-      else{
-      let fields = undefined
-      if(cur_frm.doc.document_type == 'Settlement Advice'){
-        fields = {
-          label: 'Payer Name',
-          fieldname: 'payer',
-          fieldtype: 'Link',
-          options: 'Customer',
-          description: 'if you want to map all the files with this payer name'
-        }
-        }
-      if(cur_frm.doc.document_type == 'Bank Statement'){
-        fields =  {
-          label: 'Bank Account',
-          fieldname: 'bank_account',
-          fieldtype: 'Link',
-          options: 'Bank Account',
-          description: 'if you want to map all the files with this bank account'
-      }
-      }
-      let extract = new frappe.ui.Dialog({
-        'title' : 'Extract Configuration',
-        'fields' : [
-            fields            
-        ],
-        primary_action_label: 'Extract',
-        primary_action(values) {
-          console.log(values)
-          let data = null
-            if(cur_frm.doc.document_type == 'Settlement Advice' && (typeof(values.payer) != 'undefined')){
-                data = values.payer
-            }
-            else if(cur_frm.doc.document_type == 'Bank Statement' && typeof(values.bank_account) != 'undefined'){
-                data = values.bank_account
-            }
-            else{
-                data = null
-            }
-            frappe.call({
-            method: 'agarwals.agarwals.doctype.file_upload.file_upload.run_extractor',
-            args: {
-              fid: cur_frm.docname,
-              ffield: data
-            },
-            freeze: true,
-            freeze_message: "Files are Extracting....",
-            callback: function (r) {
-              if (r.message) {
-                frappe.msgprint(r.message)
-              }
-            }
-          });
-          extract.hide()
-        }
-    })
-
-    extract.show()
-    }},  __('Action'));
-  }
-}
-
-function process_zip(frm) {
-  if (frm.doc.status == 'Zip') {
-    frm.add_custom_button(__('Process Zip'), function (frm) {
-      frappe.confirm(
+frappe.ui.form.on('File upload', {
+    }
+    }
+);
+function process(frm) {
+	frappe.confirm(
         'Are you sure you want to perform this action ?',
         function (frm) {
           frappe.call({
@@ -112,35 +31,72 @@ function process_zip(frm) {
             freeze: true,
             freeze_message: "Files are uploading....",
             callback: function (r) {
+			frm.doc.refresh()
               if (r.message) {
                 frappe.msgprint(r.message)
+			
               }
             }
           });
-        },
-        function () {}
-      );
-    }, __('Action'));
-  }
-}
+})
 
+}
 frappe.ui.form.on('File upload', {
+    extract: function(frm) {
+            extract(frm);	
+        },process: function(frm) {
+            process(frm);
+        
+    }
+}
+); 
+function extract(frm) {
+          let data = null
+            frappe.call({
+            method: 'agarwals.agarwals.doctype.file_upload.file_upload.run_extractor',
+            args: {
+              fid: cur_frm.docname,
+              ffield: data
+            },
+            freeze: true,
+            freeze_message: "Files are Extracting....",
+			callback: function (r) {
+              if (r.message) {
+				   frappe.msgprint(r.message)
+				  }
+				}
+          });  
+        }
+frappe.ui.form.on('File upload',{
   onload: function (frm) {
-    extract_zip(frm)
-    process_zip(frm)
     verify_read_only(frm)
     set_css()
   },
   refresh: function (frm) {
 	change_zip_status_color()
-    extract_zip(frm)
-    process_zip(frm)
+	update_payer_button(frm)
     verify_read_only(frm)
     set_css()
   }
 });
 
-// private button 
+function update_payer_button(frm){
+	var addButton = document.createElement('button');
+	addButton.innerText = 'Update';
+	addButton.className = 'btn btn-xs btn-secondary grid-add-row';
+	
+	addButton.addEventListener('click', function() {
+		 update_payer(frm)
+	});
+	if(frm.doc.document_type=="Bank Statement"){
+	var gridButtons = frm.fields_dict['mapping_bank'].grid.wrapper.find('.grid-buttons');
+	gridButtons.append(addButton);
+	}
+	if(frm.doc.document_type=="Settlement Advice"){
+		var gridButtons = frm.fields_dict['mapping_advice'].grid.wrapper.find('.grid-buttons');
+	    gridButtons.append(addButton);
+}
+}
 var style = document.createElement('style');
 style.textContent = `
   .btn.btn-secondary.btn-sm.btn-modal-secondary {
@@ -162,3 +118,59 @@ var change_zip_status_color=function(){
 		  code.style.color="Red"
 	 } 
 }
+function update_payer(frm){
+	if(frm.doc.document_type=="Settlement Advice"){
+	const child_rows=frm.get_selected()
+	if(Object.keys(child_rows).length>0){
+	let d = new frappe.ui.Dialog({
+        title: 'Payer details',
+        fields: [
+            {
+                label: 'Payer',
+                fieldname: 'payer',
+                fieldtype: 'Link',
+				options:'Customer'
+            }
+        ],
+        size: 'small',
+        primary_action_label: 'Submit',
+        primary_action(values) {
+				var child_row = child_rows.mapping_advice
+	           for(var i=0;i<child_row.length;i++){
+				frappe.model.set_value("Settlement Advice Mapping",child_row[i],"payer_name",values.payer)
+				frm.refresh_field("mapping_advice")
+			   }
+		   d.hide();
+        }
+    });
+    d.show();
+}}
+if(frm.doc.document_type=="Bank Statement"){
+	const child_rows=frm.get_selected()
+	console.log(child_rows)
+	if(Object.keys(child_rows).length>0){
+	let d = new frappe.ui.Dialog({
+        title: 'Bank Account details',
+        fields: [
+            {
+                label: 'Bank Account',
+                fieldname: 'bank',
+                fieldtype: 'Link',
+				options:'Bank Account'
+            }
+        ],
+        size: 'small',
+        primary_action_label: 'Submit',
+        primary_action(values) {
+				var child_row = child_rows.mapping_bank
+	           for(var i=0;i<child_row.length;i++){
+				frappe.model.set_value("Bank Account Mapping",child_row[i],"bank_account",values.bank)
+				frm.refresh_field("mapping_bank")
+			   }
+		   d.hide();
+        }
+    });
+    d.show();
+}}
+}
+	
