ea550fedfc9df7aa3c58b153caa1001010297d33
Kaarthik97 <kaarthik.n@techfinite.com>
2024-05-16 17:54:29 +0530

    error handled and validatted(sales_invoice,bank_transaction) writeoff/writeback

--- agarwals/utils/writeback_writeoff.py
+++ agarwals/utils/writeback_writeoff.py
@@ -4,33 +4,49 @@ from datetime import datetime
 
 @frappe.whitelist()
 def create_writeback_jv():
-    writeback_list = get_doc_list("Write Back", {"status": ["=", ["Created"]]}, ["*"])
+    writeback_list = get_doc_list("Write Back", {"status": "Created"}, ["*"])
     for writeback in writeback_list:
         try:
-            file_upload_name = writeback.file_upload
-            file_upload_list = get_doc_list("File upload",{"name":file_upload_name},["*"])
-            posting_date = file_upload_list[0].wb_date
-            account_name = writeback.bank_account
-            company_account_list = get_doc_list("Bank Account",{"name":account_name},["*"])
-            company_account = company_account_list[0].account
-            jv = JournalUtils()
-            je = create_journal_entry("Journal Entry", str(posting_date), writeback.reference_number)
-            credit_data, debit_data = set_writeback_account_data(writeback, company_account)
-            append_child_table = add_account_entries(je,credit_data, debit_data)
-            jv.save_je(append_child_table)
-            writeback_doc = frappe.get_doc("Write Back", writeback.name)
-            writeback_doc.status = "Processed"
-            writeback_doc.save()
+            bank_transaction = frappe.get_doc("Bank Transaction", writeback.reference_number)
+            if bank_transaction.status == "Unreconciled" or "Pending":
+                if bank_transaction.unallocated_amount <= int(writeback.unallocated_amount) + 10:
+                    file_upload_name = writeback.file_upload
+                    file_upload_list = get_doc_list("File upload",{"name":file_upload_name},["*"])
+                    posting_date = file_upload_list[0].wb_date
+                    account_name = writeback.bank_account
+                    company_account_list = get_doc_list("Bank Account",{"name":account_name},["*"])
+                    company_account = company_account_list[0].account
+                    jv = JournalUtils()
+                    je = create_journal_entry("Journal Entry", str(posting_date), writeback.reference_number)
+                    credit_data, debit_data = set_writeback_account_data(writeback, company_account)
+                    append_child_table = add_account_entries(je,credit_data, debit_data)
+                    jv.save_je(append_child_table)
+                    writeback_doc = frappe.get_doc("Write Back", writeback.name)
+                    writeback_doc.status = "Processed"
+                    writeback_doc.save()
+                else:
+                    log_error('Journal Entry', writeback.name,"Bank Transaction is Fully Reconciled")
+                    update_doc_status("Write Back", writeback.name, "Error")
+            else:
+                log_error('Journal Entry', writeback.name, "Unallocated amount in Bank Transaction is higher than the amount given in Write Back")
+                update_doc_status("Write Back", writeback.name, "Error")
         except Exception as e:
-            writeback_doc = frappe.get_doc("Write Back", writeback.name)
-            writeback_doc.status = "Error"
-            writeback_doc.save()
-            error_log = frappe.new_doc('Error Record Log')
-            error_log.set('doctype_name', 'Journal Entry')
-            error_log.set('reference_name', writeback.name)
-            error_log.set('error_message', '' + str(e))
-            error_log.save()
+            log_error('Journal Entry',writeback.name,e)
+            update_doc_status("Write Back", writeback.name, "Error")
 
+
+
+def log_error(doctype,ref_name,error_message):
+    error_log = frappe.new_doc('Error Record Log')
+    error_log.set('doctype_name', doctype)
+    error_log.set('reference_name', ref_name)
+    error_log.set('error_message', '' + str(error_message))
+    error_log.save()
+
+def update_doc_status(doctype,docname, status):
+    document = frappe.get_doc(doctype, docname)
+    document.status = status
+    document.save()
 def set_writeback_account_data(writeback,company_account):
     credit_data = {
                 'account': company_account,
@@ -61,32 +77,36 @@ def get_doc_list(doctype,filters,fields):
 
 @frappe.whitelist()
 def create_writeoff_jv():
-    writeoff_list = get_doc_list("Write Off", {"status": ["=", ["Created"]]}, ["*"])
+    writeoff_list = get_doc_list("Write Off", {"status": "Created"}, ["*"])
     for writeoff in writeoff_list:
         try:
-            file_upload_name = writeoff.file_upload
-            file_upload_list = get_doc_list("File upload",{"name":file_upload_name},["*"])
-            posting_date = file_upload_list[0].wo_date
-            debt_account = "Debtors - A"
-            jv = JournalUtils()
-            je = create_journal_entry("Journal Entry", str(posting_date), writeoff.bill_no)
-            je.bill_no = writeoff.name
-            je.bill_date = writeoff.bill_date
-            credit_data, debit_data = set_writeoff_account_data(writeoff, debt_account)
-            append_child_table = add_account_entries(je, credit_data, debit_data)
-            jv.save_je(append_child_table)
-            writeoff_doc = frappe.get_doc("Write Off", writeoff.name)
-            writeoff_doc.status = "Processed"
-            writeoff_doc.save()
+            sales_invoice = frappe.get_doc("Sales Invoice", writeoff.name)
+            if sales_invoice.status == "Unpaid" or "Partly Paid" :
+                if sales_invoice.outstanding_amount <= int(writeoff.outstanding_amount) + 10:
+                    file_upload_name = writeoff.file_upload
+                    file_upload_list = get_doc_list("File upload",{"name":file_upload_name},["*"])
+                    posting_date = file_upload_list[0].wo_date
+                    debt_account = "Debtors - A"
+                    jv = JournalUtils()
+                    je = create_journal_entry("Journal Entry", str(posting_date), writeoff.bill_no)
+                    je.bill_no = writeoff.name
+                    je.bill_date = writeoff.bill_date
+                    credit_data, debit_data = set_writeoff_account_data(writeoff, debt_account)
+                    append_child_table = add_account_entries(je, credit_data, debit_data)
+                    jv.save_je(append_child_table)
+                    writeoff_doc = frappe.get_doc("Write Off", writeoff.name)
+                    writeoff_doc.status = "Processed"
+                    writeoff_doc.save()
+                else:
+                    log_error('Journal Entry', writeoff.name,
+                              "Sales Invoice Tagged to bill is Paid or Cancelled")
+                    update_doc_status("Write Back", writeoff.name, "Error")
+            else:
+                log_error('Journal Entry', writeoff.name, "Unallocated Amount in Sales Invoice is Higher than the Amount given in Write Off")
+                update_doc_status("Write Back", writeoff.name, "Error")
         except Exception as e:
-            writeoff_doc = frappe.get_doc("Write Off", writeoff.name)
-            writeoff_doc.status = "Error"
-            writeoff_doc.save()
-            error_log = frappe.new_doc('Error Record Log')
-            error_log.set('doctype_name', 'Journal Entry')
-            error_log.set('reference_name', writeoff.name)
-            error_log.set('error_message', '' + str(e))
-            error_log.save()
+            log_error("Journal Entry", writeoff.name,e)
+            update_doc_status("Write Off", writeoff.name, "Error")
 
 def set_writeoff_account_data(writeoff,debt_account):
     credit_data = {
