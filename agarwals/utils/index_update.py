3dffa1d79b2fd04ef1e11b1e74fe02cb300ccdd4
Gokul-Annadurai <gokul.a@techfinite.com>
2024-05-24 16:18:33 +0530

    index updation space added

--- agarwals/utils/index_update.py
+++ agarwals/utils/index_update.py
@@ -2,11 +2,11 @@ import frappe
 
 @frappe.whitelist()
 def update_index():
-	indexes=frappe.get_all('Index Updation')
+	indexes = frappe.get_all('Index Updation')
 	for index in indexes:
-		index_doc=frappe.get_doc('Index Updation',index['name'])
-		table=index_doc.doctype_name
-		columns=(index_doc.indexing_columns).split(",")
+		index_doc = frappe.get_doc('Index Updation',index['name'])
+		table = index_doc.doctype_name
+		columns = (index_doc.indexing_columns).split(",")
 		for column in columns:
-			frappe.db.sql("""CREATE INDEX IF NOT EXISTS {index_column}_cg ON `tab{table}` ({column})""".format(index_column=column,table=table,column=column))
+			frappe.db.sql("""CREATE INDEX IF NOT EXISTS {index_column}_cg ON `tab{table}` ({column})""".format(index_column = column,table = table,column = column))
 	
