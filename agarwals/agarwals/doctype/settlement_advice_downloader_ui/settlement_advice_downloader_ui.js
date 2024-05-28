e48a6edeb19a499cacf7121cbd6fd0014e9a0fde
Kavi-25 <kramalingam@techfinite.com>
2024-05-28 18:46:42 +0530

    removed realtime on

--- agarwals/agarwals/doctype/settlement_advice_downloader_ui/settlement_advice_downloader_ui.js
+++ agarwals/agarwals/doctype/settlement_advice_downloader_ui/settlement_advice_downloader_ui.js
@@ -57,16 +57,16 @@ frappe.ui.form.on('Settlement Advice Downloader UI', {
 
 
 });
-frappe.realtime.on("eventsts", function(response) {
-	console.log("websocket Triggered")
-	if (cur_frm && cur_frm.doc.doctype === 'Settlement Advice Downloader UI') {
-		console.log("doctype identified")
-        cur_frm.reload_doc();
-    }
-	else{
-		console.log(' Could not find the doctype ')
-	}
-});
+// frappe.realtime.on("eventsts", function(response) {
+// 	console.log("websocket Triggered")
+// 	if (cur_frm && cur_frm.doc.doctype === 'Settlement Advice Downloader UI') {
+// 		console.log("doctype identified")
+//         cur_frm.reload_doc();
+//     }
+// 	else{
+// 		console.log(' Could not find the doctype ')
+// 	}
+// });
 
 
 
