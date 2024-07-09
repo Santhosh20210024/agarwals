ed0016af5987c55850c2fc3aa646e6f8feaaf82a
prem-techfinite <premkumar.g@techfinite.com>
2024-07-09 14:01:32 +0530

    patch fix on sales invoice Reference child table deletion

--- agarwals/reconciliation/step/sales_invoice_creator.py
+++ agarwals/reconciliation/step/sales_invoice_creator.py
@@ -2,10 +2,10 @@ import frappe
 from agarwals.agarwals.doctype import file_records
 from agarwals.utils.payment_entry_cancellator import PaymentEntryCancellator
 from agarwals.utils.journal_entry_cancellator import JournalEntryCancellator
+from agarwals.utils.Matcher_Reference_Cancellator import MatcherReferenceCancellator
 from agarwals.reconciliation import chunk
 from agarwals.utils.str_to_dict import cast_to_dic
 from agarwals.utils.error_handler import log_error
-from agarwals.utils.fiscal_year_update import update_fiscal_year
 
 class SalesInvoiceCreator:
     def cancel_sales_invoice(self, cancelled_bills):
@@ -17,14 +17,18 @@ class SalesInvoiceCreator:
                 sales_invoice_record.custom_transform = bill_record.transform
                 sales_invoice_record.custom_index = bill_record.index
                 sales_invoice_record.save(ignore_permissions=True)
+                journal_entry_doc = frappe.get_all('Journal Entry Account', filters={'reference_name': bill, 'reference_type':'Sales Invoice'}, pluck = 'parent')
                 sales_invoice_record.cancel()
                 PaymentEntryCancellator().cancel_payment_entry(bill)
-                JournalEntryCancellator().cancel_journal_entry(bill)
+                if journal_entry_doc:
+                    JournalEntryCancellator().cancel_journal_entry(journal_entry_doc)
+                MatcherReferenceCancellator().delete_matcher_reference(bill)
                 frappe.db.set_value('Bill', bill, {'invoice_status': 'CANCELLED'})
+                self.delete_sales_invoice_reference(bill)
                 frappe.db.commit()
-                file_records.create(file_upload=sales_invoice_record.file_upload,
-                                    transform=sales_invoice_record.transform, reference_doc=sales_invoice_record.doctype,
-                                    record=bill, index=sales_invoice_record.index)
+                file_records.create(file_upload=sales_invoice_record.custom_file_upload,
+                                    transform=sales_invoice_record.custom_transform, reference_doc=sales_invoice_record.doctype,
+                                    record=bill, index=sales_invoice_record.custom_index)
             except Exception as e:
                 error_log = frappe.new_doc('Error Record Log')
                 error_log.set('doctype_name', 'Bill')
@@ -32,6 +36,10 @@ class SalesInvoiceCreator:
                 error_log.set('error_message', e)
                 error_log.save()
 
+    def delete_sales_invoice_reference(self, bill):
+        frappe.db.sql(f"DELETE FROM `tabSales Invoice Reference` WHERE parent = '{bill}'")
+        
+        
     def process(self, bill_numbers, chunk_doc):
         chunk.update_status(chunk_doc, "InProgress")
         try:
@@ -57,14 +65,12 @@ class SalesInvoiceCreator:
                                             'custom_index': bill_record.index}
                     for key, value in sales_invoice_params.items():
                         sales_invoice_record.set(key, value)
-                    update_fiscal_year(sales_invoice_record,'Sales Invoice')
                     sales_invoice_record.save()
                     sales_invoice_record.submit()
                     if bill_record.status == 'CANCELLED':
                         sales_invoice_record.cancel()
                     frappe.db.set_value('Bill', bill_number, {'invoice': sales_invoice_record.name, 'invoice_status': bill_record.status})
                     frappe.db.commit()
-                    
                     file_records.create(file_upload=sales_invoice_record.custom_file_upload,
                                         transform=sales_invoice_record.custom_transform, reference_doc=sales_invoice_record.doctype,
                                         record=bill_number, index=sales_invoice_record.custom_index)
@@ -88,27 +94,27 @@ class SalesInvoiceCreator:
 @frappe.whitelist()
 def process(args):
     try:
-        args = cast_to_dic(args)
+        args=cast_to_dic(args)
         cancelled_bills = frappe.get_list('Bill', filters={'status': 'CANCELLED', 'invoice_status': 'RAISED'},
                                           pluck='name')
-        cancellation_chunk_doc = chunk.create_chunk(args["step_id"])
         if cancelled_bills:
-            chunk.update_status(cancellation_chunk_doc, "InProgress")
+            chunk_doc = chunk.create_chunk(args["step_id"])
+            chunk.update_status(chunk_doc, "InProgress")
             try:
                 SalesInvoiceCreator().cancel_sales_invoice(cancelled_bills)
-                cancellation_chunk_doc_status = "Processed"
+                chunk.update_status(chunk_doc, "Processed")
             except Exception as e:
-                cancellation_chunk_doc_status = "Error"
-        else:
-            cancellation_chunk_doc_status = "Processed"
-        new_bills = frappe.get_list('Bill', filters={'invoice': '', 'status': ['!=','CANCELLED AND DELETED']},pluck = 'name')
-        if new_bills:
-            SalesInvoiceCreator().enqueue_jobs(new_bills, args)
+                chunk.update_status(chunk_doc, "Error")
         else:
             chunk_doc = chunk.create_chunk(args["step_id"])
             chunk.update_status(chunk_doc, "Processed")
-        chunk.update_status(cancellation_chunk_doc, cancellation_chunk_doc_status)
+        new_bills = frappe.get_list('Bill', filters={'invoice': '', 'status': ['!=','CANCELLED AND DELETED']},pluck = 'name')
+        if new_bills:
+            SalesInvoiceCreator().enqueue_jobs(new_bills,args)
+        else:
+            chunk_doc = chunk.create_chunk(args["step_id"])
+            chunk.update_status(chunk_doc, "Processed")
     except Exception as e:
         chunk_doc = chunk.create_chunk(args["step_id"])
         chunk.update_status(chunk_doc, "Error")
-        log_error(e, 'Step')
+        log_error(e,'Step')
