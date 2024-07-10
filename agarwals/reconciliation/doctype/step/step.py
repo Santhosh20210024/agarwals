9c1e20d07d1f10887d26f2871d76e33f9d3c8e55
SAnnamalai-techfinite <SAnnamalai@techfinite.com>
2024-07-10 13:37:54 +0530

    update the publish error in worker added the test method

--- agarwals/reconciliation/doctype/step/step.py
+++ agarwals/reconciliation/doctype/step/step.py
@@ -6,9 +6,8 @@ from frappe.model.document import Document
 from agarwals.reconciliation.step.step_processor import StepProcessor
 from agarwals.reconciliation.job.processor import Processor
 
-
 class Step(Document, Processor):
-	    
+
 	def on_update(self):
 		StepProcessor().process(self.name, self.job_id)
 			
