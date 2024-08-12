cc951afb8642e0c1b80449fe47bc0336990651fa
Kavi-25 <kramalingam@techfinite.com>
2024-08-12 15:11:01 +0530

    error handler bug fixed

--- agarwals/utils/error_handler.py
+++ agarwals/utils/error_handler.py
@@ -1,7 +1,7 @@
 import frappe
 
 def log_error(error,doc=None,doc_name=None):
-    if len(doc_name) > 140:
+    if doc_name and len(doc_name) > 140:
         doc_name = doc_name[:139]
     error_log = frappe.new_doc('Error Record Log')
     error_log.set('doctype_name', doc)
