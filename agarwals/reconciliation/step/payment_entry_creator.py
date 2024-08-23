fc0c679ed045f036911bf8a976890f1beb3d7fe7
SAnnmalai-techfinite <SAnnmalai@techfinite.com>
2024-08-23 19:15:40 +0530

    Removed the timer

--- agarwals/reconciliation/step/payment_entry_creator.py
+++ agarwals/reconciliation/step/payment_entry_creator.py
@@ -4,7 +4,6 @@ from agarwals.reconciliation import chunk
 from agarwals.utils.str_to_dict import cast_to_dic
 from agarwals.utils.error_handler import log_error
 from frappe.model.document import Document
-from tfs.profiler.timer import Timer
 from datetime import date
 from agarwals.utils.reconciliation_utils import update_error, get_document_record, get_posting_date, get_entity_closing_date
 
@@ -13,7 +12,6 @@ chunk_status: str = "Processed"
 
 class PaymentEntryCreator:
     def __init__(self, matcher_record: "Document", bt_doc: "Document"):
-        class_init_timer = Timer().start(f"Payment_Entry_class_init {matcher_record.name}")
         self.matcher_record = matcher_record
         self.settled_amount: float = round(float(matcher_record.settled_amount),
                                            2) if matcher_record.settled_amount else 0
@@ -25,10 +23,8 @@ class PaymentEntryCreator:
         self.sa_remark: str = ''
         self.pe_doc = None
         self.bt_doc = bt_doc
-        class_init_timer.end()
 
     def __validate(self) -> bool:
-        validate_timer = Timer().start(f"validate {self.matcher_record.name}")
         self.si_doc: "Document" = get_document_record('Sales Invoice', self.matcher_record.sales_invoice)
         error = None
         if self.si_doc.status == 'Cancelled':
@@ -39,9 +35,7 @@ class PaymentEntryCreator:
             error = 'Claim amount lesser than the cumulative of other amounts'
         if error:
             update_error(self.matcher_record, error)
-            validate_timer.end()
             return False
-        validate_timer.end()
         return True
 
     def __set_sa_vars(self, status: str = 'Partially Processed', remark: str = '') -> None:
@@ -49,7 +43,6 @@ class PaymentEntryCreator:
         self.sa_remark = remark
 
     def __set_amount(self) -> None:
-        set_amount_timer = Timer().start(f"set_amount {self.matcher_record.name}")
         unallocated_amount: float = float(self.bt_doc.unallocated_amount)
         si_outstanding: float = float(self.si_doc.outstanding_amount)
         if self.settled_amount > unallocated_amount:
@@ -68,22 +61,17 @@ class PaymentEntryCreator:
             self.tds_amount = 0
             self.disallowance_amount = 0
             self.__set_sa_vars(remark='Both Disallowed and TDS amount is greater than Outstanding Amount')
-        set_amount_timer.end()
 
     def __get_entry_name(self) -> str:
-        get_entry_name_timer = Timer().start(f"get_entry_name {self.si_doc.name}")
         existing_payment_entries = frappe.get_list('Payment Entry'
                                                    , filters={'custom_sales_invoice': self.si_doc.name})
         name = self.si_doc.name + "-" + str(
             len(existing_payment_entries)) if existing_payment_entries else self.si_doc.name
-        get_entry_name_timer.end()
         return name
 
     def __get_entity_posting_date(self) -> date:
-        get_posting_date_timer = Timer().start(f"get_posting_date {self.matcher_record.name}")
         entity_closing_date = get_entity_closing_date(self.matcher_record.si_entity)
         posting_date = get_posting_date(self.bt_doc.date, entity_closing_date)
-        get_posting_date_timer.end()
         return posting_date
 
     def __add_deduction(self, account: str, description: str, amount: float) -> dict:
@@ -93,7 +81,6 @@ class PaymentEntryCreator:
                 'branch_type': self.si_doc.branch_type, 'amount': amount}
 
     def __process_write_off(self, pe_dict: dict) -> dict:
-        process_round_timer = Timer().start(f"process_write_off {pe_dict['name']} ")
         deductions: list = []
         si_outstanding_amount: float = float(self.si_doc.outstanding_amount)
         si_allocated_amount: float = pe_dict["references"][0]["allocated_amount"]
@@ -106,23 +93,15 @@ class PaymentEntryCreator:
             if "deductions" not in pe_dict.keys():
                 pe_dict["deductions"] = []
             pe_dict["deductions"] = pe_dict["deductions"] + deductions
-        process_round_timer.end()
         return pe_dict
 
     def __create_payment_entry(self, pe_dict: dict) -> "Document":
-        create_payment_timer = Timer().start(f"create_payment_entry {pe_dict['name']}")
         pe_doc = frappe.get_doc(pe_dict)
-        process_payment_entry_insert_timer = Timer().start(f"payment_entry Insert {pe_dict['name']}")
         pe_doc.insert()
-        process_payment_entry_insert_timer.end()
-        process_payment_entry_submit_timer = Timer().start(f"payment_entry_Submit {pe_dict['name']}")
         pe_doc.submit()
-        process_payment_entry_submit_timer.end()
-        create_payment_timer.end()
         return pe_doc
 
     def __process_payment_entry(self) -> None:
-        process_payment_entry_timer = Timer().start(f"process_payment_entry {self.si_doc.name}")
         try:
             party_and_bank_balance: dict = {'paid_to_account_balance': 0.01, 'paid_from_account_balance': 0.01,
                                       'party_balance': 0.01}
@@ -180,14 +159,11 @@ class PaymentEntryCreator:
             file_records.create(file_upload=self.pe_doc.custom_file_upload,
                                 transform=self.pe_doc.custom_transform, reference_doc=self.pe_doc.doctype,
                                 record=self.pe_doc.name, index=self.pe_doc.custom_index)
-            process_payment_entry_timer.end()
         except Exception as e:
             self.__set_sa_vars("Warning", 'Unable to Create Payment Entry')
-            process_payment_entry_timer.end()
             raise Exception(e)
 
     def __update_trans_reference(self) -> None:
-        update_trans_timer = Timer().start(f"update_trans_reference {self.bt_doc.name}")
         self.bt_doc.append('payment_entries',
                            {'payment_document': self.pe_doc.doctype
                                , 'payment_entry': self.pe_doc.name
@@ -198,13 +174,9 @@ class PaymentEntryCreator:
                                , 'custom_bill_branch_type': self.pe_doc.branch_type
                                , 'custom_bill_entity': self.pe_doc.entity})
         self.bt_doc.custom_advice_status = 'Found'
-        update_trans_submit_timer = Timer().start(f"update_trans_reference_submit {self.bt_doc.name}")
         self.bt_doc.submit()
-        update_trans_submit_timer.end()
-        update_trans_timer.end()
 
     def __update_advice_reference(self) -> None:
-        update_advice_reference_timer = Timer().start(f"update_advice_reference {self.matcher_record.name}")
         if not self.matcher_record.settlement_advice:
             return None
         settlement_advice: "Document" = get_document_record('Settlement Advice', self.matcher_record.settlement_advice)
@@ -222,23 +194,17 @@ class PaymentEntryCreator:
                 'insurance_company_name'] = self.matcher_record.claimbook, self.matcher_record.insurance_company_name
         settlement_advice.update(sa_dict)
         settlement_advice.save()
-        update_advice_reference_timer.end()
 
     def __update_claim_reference(self) -> None:
-        update_claim_referece = Timer().start(f"update_claim_reference {self.matcher_record.name}")
         frappe.db.sql(
             f"""UPDATE `tabClaimBook` SET matched_status = 'Matched' WHERE name = \"{self.matcher_record.claimbook}\"""")
-        update_claim_referece.end()
 
     def __update_references(self) -> None:
-        update_reference_timer = Timer().start(f"update_reference {self.matcher_record.name}")
         self.__update_trans_reference()
         self.__update_advice_reference()
         self.__update_claim_reference()
-        update_reference_timer.end()
 
     def process(self) -> None:
-        Payment_creation_process_timer = Timer().start(f"Payment_creation_process {self.matcher_record.name}")
         try:
             if not self.__validate():
                 return
@@ -254,7 +220,6 @@ class PaymentEntryCreator:
         finally:
             self.matcher_record.save()
             frappe.db.commit()
-            Payment_creation_process_timer.end()
 
 
 class BankReconciliation:
@@ -262,7 +227,6 @@ class BankReconciliation:
         self.bt_doc = None
 
     def __validate(self, matcher_record: "Document") -> bool:
-        validate_timer = Timer().start(f"Reconciliation validate {matcher_record.name}")
         error = None
         if self.bt_doc.status == 'Reconciled':
             error = 'Already Reconciled'
@@ -270,49 +234,36 @@ class BankReconciliation:
             error = 'Status Should be other then Pending, Unreconciled'
         if error:
             update_error(matcher_record, error)
-            validate_timer.end()
             return False
-        validate_timer.end()
         return True
 
     def process(self, bt: dict) -> None:
-        t1 = Timer().start(f"Reconciliation process {bt.bank_transaction}")
         try:
             self.bt_doc: "Document" = get_document_record("Bank Transaction", bt.bank_transaction)
             matcher_name_list: list[str] = eval(bt.matcher_names)
             for matcher_name in matcher_name_list:
-                t2 = Timer().start(f"Reconciliation Matcher Loop {matcher_name}")
                 matcher_doc: "Document" = get_document_record("Matcher", matcher_name)
                 if not self.__validate(matcher_doc):
-                    t2.end()
                     continue
                 PaymentEntryCreator(matcher_doc, self.bt_doc).process()
                 self.bt_doc.reload()
-                t2.end()
         except Exception as e:
             global chunk_status
             chunk_status = "Error"
             log_error(e, 'Matcher')
-        finally:
-            t1.end()
 
 
 def reconcile_bank_transaction(bt_records: list[dict], chunk_doc: "Document", batch: str) -> None:
-    t1 = Timer().start(f"reconcile_bank_transaction {batch}")
     chunk.update_status(chunk_doc, "InProgress")
     try:
         for bt in bt_records:
-            t2 = Timer().start(f"BankReconciliation {bt.bank_transaction}")
             BankReconciliation().process(bt)
-            t2.end()
     except Exception as e:
         global chunk_status
         chunk_status = "Error"
         log_error(e, 'Matcher')
     finally:
         chunk.update_status(chunk_doc, chunk_status)
-        t1.end()
-        t1.print(batch)
 
 
 @frappe.whitelist()
@@ -325,21 +276,21 @@ def process(args):
         bt_records = frappe.db.sql("""SELECT bank_transaction, JSON_ARRAYAGG(name) as matcher_names
                                         FROM `tabMatcher`
                                         where match_logic in  %(m_logic)s and status = 'Open'
-                                        GROUP BY bank_transaction ORDER BY sales_invoice, payment_order, unallocated DESC"""
-                                   , values={"m_logic": m_logic}
+                                        GROUP BY bank_transaction ORDER BY sales_invoice, payment_order, unallocated DESC LIMIT %(limit)s"""
+                                   , values={"m_logic": m_logic, "limit": chunk_size}
                                    , as_dict=True)
         if bt_records:
             for record in range(0, len(bt_records), chunk_size):
                 chunk_doc = chunk.create_chunk(args["step_id"])
                 seq_no = seq_no + 1
-                # reconcile_bank_transaction(bt_records=bt_records, chunk_doc=chunk_doc, batch = "Batch" + str(seq_no))
-                frappe.enqueue(reconcile_bank_transaction
-                               , queue='long'
-                               , is_async=True
-                               , job_name="Batch" + str(seq_no)
-                               , timeout=25000
-                               , bt_records=bt_records[record:record + chunk_size]
-                               , chunk_doc=chunk_doc, batch="Batch" + str(seq_no))
+                reconcile_bank_transaction(bt_records=bt_records, chunk_doc=chunk_doc, batch = "Batch" + str(seq_no))
+                # frappe.enqueue(reconcile_bank_transaction
+                #                , queue='long'
+                #                , is_async=True
+                #                , job_name="Batch" + str(seq_no)
+                #                , timeout=25000
+                #                , bt_records=bt_records[record:record + chunk_size]
+                #                , chunk_doc=chunk_doc, batch="Batch" + str(seq_no))
         else:
             chunk_doc = chunk.create_chunk(args["step_id"])
             chunk.update_status(chunk_doc, "Processed")
--- agarwals/utils/reconciliation_utils.py
+++ agarwals/utils/reconciliation_utils.py
@@ -1,6 +1,5 @@
 from datetime import date, datetime
 import frappe
-from tfs.profiler.timer import Timer
 from frappe.utils.caching import redis_cache
 from frappe import utils
 from frappe.model.document import Document
@@ -8,26 +7,20 @@ from agarwals.utils.error_handler import log_error
 
 
 def get_document_record(doctype: str, name: str) -> "Document":
-    document_timer = Timer().start(f"get_document_record {doctype} - {name}")
     doc: "Document" = frappe.get_doc(doctype, name)
-    document_timer.end()
     return doc
 
 
 def update_advice_log(advice: str, status: str, msg: str) -> None:
-    advice_log_timer = Timer().start(f"update_advice_log {advice}")
     settlement_advice: "Document" = get_document_record('Settlement Advice', advice)
     settlement_advice.status = status
     settlement_advice.remark = msg
     settlement_advice.save()
-    advice_log_timer.end()
 
 
 def update_matcher_log(matcher_record: "Document", status: str, msg: str) -> None:
-    macher_log_timer = Timer().start(f"update_matcher_log {matcher_record.name}")
     matcher_record.status = status
     matcher_record.remarks = msg
-    macher_log_timer.end()
 
 
 def update_error(matcher_record: "Document", message: str, error: str | Exception = None) -> None:
@@ -40,24 +33,19 @@ def update_error(matcher_record: "Document", message: str, error: str | Exceptio
 
 @redis_cache
 def get_company_account(bank_account_name: str) -> str | None:
-    get_company_timer = Timer().start(f"get_company_account {bank_account_name}")
     bank_account: "Document" = frappe.get_doc('Bank Account', bank_account_name)
     if not bank_account.account:
-        get_company_timer.end()
         return None
-    get_company_timer.end()
     return bank_account.account
 
 
 @redis_cache
 def get_entity_closing_date(entity: str) -> date | None:
-    get_posting_date_timer = Timer().start(f"get_posting_date {entity}")
     closing_date_list = frappe.get_list('Period Closure by Entity',
                                         filters={'entity': entity}
                                         , order_by='creation desc'
                                         , pluck='posting_date')
     closing_date = max(closing_date_list) if closing_date_list else None
-    get_posting_date_timer.end()
     return closing_date
 
 
