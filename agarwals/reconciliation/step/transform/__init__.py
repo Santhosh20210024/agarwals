74a5c0d0f132cee9af2d384d1efcecd995dea43e
SAnnmalai-techfinite <SAnnmalai@techfinite.com>
2024-08-31 19:01:39 +0530

    changed the transformer with the new orchestrator

--- agarwals/reconciliation/step/transform/__init__.py
+++ agarwals/reconciliation/step/transform/__init__.py
@@ -2,13 +2,15 @@ from agarwals.factory.transformer_factory import get_transformer
 import frappe
 from agarwals.utils.str_to_dict import cast_to_dic
 from agarwals.utils.error_handler import log_error
+from tfs.orchestration import Orchestrator
+
 
 @frappe.whitelist()
 def process(args):
     try:
         args=cast_to_dic(args)
         transformer = get_transformer(args["type"])
-        transformer.process(args)
+        Orchestrator().process(transformer.process, step_id=args["step_id"])
         return "Success"
     except Exception as e:
         log_error(e,'Step')
