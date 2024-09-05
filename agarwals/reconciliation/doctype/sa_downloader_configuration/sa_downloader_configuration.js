fe7d745d34398fbba74dd22b8be22fc281e4d440
arunadevi_1108 <arunadevi.m@techfinite.com>
2024-09-05 11:26:46 +0530

    Added validation to Date in Sa downloader

--- agarwals/reconciliation/doctype/sa_downloader_configuration/sa_downloader_configuration.js
+++ agarwals/reconciliation/doctype/sa_downloader_configuration/sa_downloader_configuration.js
@@ -4,16 +4,22 @@
 frappe.ui.form.on('SA Downloader Configuration', {
     before_save: function (frm) {
         if (frm.doc.days <= 0) {
-            
-			frappe.msgprint({
+            frappe.msgprint({
                 title: __('Warning'),
                 indicator: 'orange',
                 message: __('The days field must be greater than zero. Setting to default value of 29.')
             });
             frm.set_value('days', 29);
-            
-            
+        }
+
+        if (frm.doc.to_date < frm.doc.from_date) {
+            frappe.msgprint({
+                title: __('Error'),
+                indicator: 'red',
+                message: __('To Date cannot be less than From Date')
+            });
+            frappe.validated = false; 
+
         }
     }
 });
-
