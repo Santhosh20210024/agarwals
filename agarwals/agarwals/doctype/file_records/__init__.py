1c8c6f39aafaef64dcb0b05a2d31e852a33639c8
Kavi-25 <kramalingam@techfinite.com>
2024-09-12 20:11:18 +0530

    before restore backup - SAFE PUSH

--- agarwals/agarwals/doctype/file_records/__init__.py
+++ agarwals/agarwals/doctype/file_records/__init__.py
@@ -1,6 +1,6 @@
 import frappe
 from agarwals.utils.error_handler import log_error
-
+from agarwals.reconciliation.job import latest_job_name
 
 def create(**kwargs):
     doc_name = "File Records"
@@ -10,6 +10,7 @@ def create(**kwargs):
         doc = frappe.new_doc(doc_name)
         for key, value in kwargs.items():
             setattr(doc, key, value)
+        doc.job = latest_job_name()
         doc.insert(ignore_permissions=True)
         frappe.db.commit()
     except Exception as e:
--- agarwals/agarwals/doctype/file_records/file_records.py
+++ agarwals/agarwals/doctype/file_records/file_records.py
@@ -3,6 +3,6 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from agarwals.reconciliation.job import latest_job_name
 class FileRecords(Document):
 	pass
