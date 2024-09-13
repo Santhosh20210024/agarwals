a34ee8addc9967f5a0dd94188aa18e5adcac6b68
Gokul-Annadurai <gokul.a@techfinite.com>
2024-09-13 19:35:25 +0530

    "net outstanding report"

---
+++ agarwals/agarwals/report/net_outstanding_report/net_outstanding_report.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, Agarwals and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Net Outstanding Report"] = {
+	"filters": [
+		{
+			"fieldname": "region",
+			"label": __("Region"),
+			"fieldtype": "MultiSelectList",
+			"options": "Region",
+			"get_data": function(txt) {
+				return frappe.db.get_link_options('Region', txt);
+			}
+		}
+	]
+};
