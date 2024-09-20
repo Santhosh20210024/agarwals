22c09b8fbcc2c5dc8d0f27c7e5b51d626748d12e
Gokul-Annadurai <gokul.a@techfinite.com>
2024-09-20 05:54:15 +0000

    "changes in net outstanding report"

--- agarwals/agarwals/report/net_outstanding_report/net_outstanding_report.py
+++ agarwals/agarwals/report/net_outstanding_report/net_outstanding_report.py
@@ -89,6 +89,7 @@ def execute(filters=None):
     FROM
         `viewSales Invoice Report 24-25 with Row Number` vsir
     WHERE vsir.`Status` NOT IN ('Paid','Cancelled') and vsir.`Region` IN %(region)s 
+    GROUP BY vsir.`Bill Number`
     UNION ALL
     SELECT
         CASE
@@ -156,6 +157,7 @@ def execute(filters=None):
     FROM
         `viewSorted Current Brank Transaction` vscbt
     WHERE vscbt.`Status` NOT IN ('Reconciled','Cancelled') and vscbt.`Region`  in %(region)s
+    GROUP BY vscbt.`UTR_Number`
     """
 
     # Execute the query and fetch the data
@@ -177,11 +179,11 @@ def execute(filters=None):
         {"label": "MRN", "fieldname": "MRN", "fieldtype": "Data"},
         {"label": "Status", "fieldname": "Status", "fieldtype": "Data"},
         {"label": "Insurance Name", "fieldname": "Insurance_Name", "fieldtype": "Data"},
-        {"label": "Claim Amount", "fieldname": "Claim_Amount", "fieldtype": "Data"},
-        {"label": "Total Settled Amount", "fieldname": "Total_Settled_Amount", "fieldtype": "Data"},
-        {"label": "Total TDS Amount", "fieldname": "Total_TDS_Amount", "fieldtype": "Data"},
-        {"label": "Total Disallowance Amount", "fieldname": "Total_Disallowance_Amount", "fieldtype": "Data"},
-        {"label": "Outstanding Amount", "fieldname": "Outstanding_Amount", "fieldtype": "Data"},
+        {"label": "Claim Amount", "fieldname": "Claim_Amount", "fieldtype": "Currency"},
+        {"label": "Total Settled Amount", "fieldname": "Total_Settled_Amount", "fieldtype": "Currency"},
+        {"label": "Total TDS Amount", "fieldname": "Total_TDS_Amount", "fieldtype": "Currency"},
+        {"label": "Total Disallowance Amount", "fieldname": "Total_Disallowance_Amount", "fieldtype": "Currency"},
+        {"label": "Outstanding Amount", "fieldname": "Outstanding_Amount", "fieldtype": "Currency"},
         {"label": "Type", "fieldname": "Type", "fieldtype": "Data"}
     ]
 
