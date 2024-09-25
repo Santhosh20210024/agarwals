f8e2f0ab7ceba2481d4ff1689bdc162865271709
Gokul-Annadurai <gokul.a@techfinite.com>
2024-09-25 11:49:24 +0530

    net outstanding report

--- agarwals/agarwals/report/net_outstanding_report/net_outstanding_report.js
+++ agarwals/agarwals/report/net_outstanding_report/net_outstanding_report.js
@@ -3,24 +3,49 @@
 /* eslint-disable */
 
 frappe.query_reports["Net Outstanding Report"] = {
-	"filters": [
-		{
-			"fieldname": "region",
-			"label": __("Region"),
-			"fieldtype": "MultiSelectList",
-			"options": "Region",
-			"get_data": function(txt) {
-				return frappe.db.get_link_options('Region', txt);
+	onload: function (report) { 
+		report.page.add_inner_button("Apply Filters", function () { 
+			report.set_filter_value('execute', 1); });
+
+		report.page.add_inner_button("Remove Filters", function (){
+		  report.set_filter_value('execute', 1);
+		  report.set_filter_value('branch',[]);
+		  report.set_filter_value('entity',[]);
+		  report.set_filter_value('party_group',[]);
+		  report.set_filter_value('bank_account',[]);
+		  report.set_filter_value('status',[]);
+		  report.set_filter_value('from_utr_date',null);
+		  report.set_filter_value('to_utr_date',null);
+		});
+	},
+		 "filters": 
+		 [
+				{
+					"fieldname": "execute", 
+					"label": __("Execute"), 
+					"fieldtype": "Check", 
+					"default": 1 ,
+					"hidden": 1
+				},
+				{
+					"fieldname": "entity",
+					"label": __("Entity"),
+					"fieldtype": "MultiSelectList",
+					"options": "Entity",
+					"get_data": function(txt) {
+						return frappe.db.get_link_options('Entity', txt);
+					},
+					"on_change": function (report) { report.set_filter_value('execute', 0) }
+				},
+			{
+				"fieldname": "region",
+				"label": __("Region"),
+				"fieldtype": "MultiSelectList",
+				"options": "Region",
+				"get_data": function(txt) {
+					return frappe.db.get_link_options('Region', txt);
+				},
+				"on_change": function (report) { report.set_filter_value('execute', 0) }
 			}
-		},
-		{
-			"fieldname": "entity",
-			"label": __("Entity"),
-			"fieldtype": "MultiSelectList",
-			"options": "Entity",
-			"get_data": function(txt) {
-				return frappe.db.get_link_options('Entity', txt);
-			}
-		}
-	]
+			] 
 };
