f16255a487bda4af4448e0bcb2ed45f2a8e5283d
Balamurugan1207 <bramachandran@techfinite.com>
2024-09-25 12:46:14 +0530

    Changed the or available ob not available script report

--- agarwals/agarwals/report/or_available___ob_not_available_script_report/or_available___ob_not_available_script_report.js
+++ agarwals/agarwals/report/or_available___ob_not_available_script_report/or_available___ob_not_available_script_report.js
@@ -3,16 +3,64 @@
 /* eslint-disable */
 
 frappe.query_reports["OR Available - OB Not Available Script Report"] = {
-	"filters": [
-		{
-            "fieldname": "from_date",
-            "label": __("From Date"),
+	"filters": [{
+            "fieldname": "from_utr_date",
+            "label": __("From UTR Date"),
             "fieldtype": "Date",
+            "on_change": function (report) {
+               report.set_filter_value('execute',0)
+           }
+        },
+		{
+            "fieldname": "to_utr_date",
+            "label": __("To UTR Date"),
+            "fieldtype": "Date",
+            "on_change": function (report) {
+               report.set_filter_value('execute',0)
+           }
         },
         {
-            "fieldname": "to_date",
-            "label": __("To Date"),
-            "fieldtype": "Date",
+            "fieldname": "bank_account",
+            "label": __("Bank Account"),
+            "fieldtype": "MultiSelectList",
+			"options": "Bank Account",
+			"get_data": function (txt){
+                return frappe.db.get_link_options('Bank Account',txt)
+            },
+            "on_change": function (report) {
+               report.set_filter_value('execute',0)
+           }
+        },
+		{
+            "fieldname": "bank_entity",
+            "label": __("Bank Entity"),
+            "fieldtype": "MultiSelectList",
+			"options": "Entity",
+			"get_data": function (txt){
+                return frappe.db.get_link_options('Entity',txt)
+            },
+            "on_change": function (report) {
+               report.set_filter_value('execute',0)
+           }
+        },
+		{
+            "fieldname": "bank_region",
+            "label": __("Bank Region"),
+            "fieldtype": "MultiSelectList",
+			"options": "Region",
+			"get_data": function (txt){
+                return frappe.db.get_link_options('Region',txt)
+            },
+            "on_change": function (report) {
+               report.set_filter_value('execute',0)
+           }
+        },
+		{
+            "fieldname":"execute",
+            "label":__("Execute"),
+            "fieldtype":"Check",
+            "default": 1,
+            "hidden": 1
         }
 
 	]
