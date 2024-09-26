597d1dba555c3e0f51393988e2b2e2cd53793595
Balamurugan1207 <bramachandran@techfinite.com>
2024-09-26 13:15:50 +0530

    Restricted the Back Date more than 7 days in Bill Entry

--- agarwals/agarwals/doctype/bill_entry/bill_entry.js
+++ agarwals/agarwals/doctype/bill_entry/bill_entry.js
@@ -23,6 +23,10 @@ frappe.ui.form.on('Bill Entry', {
     date: function (frm){
         let input_date = new Date(frm.doc.date)
         let current_date = new Date()
+        if(input_date < current_date && Math.floor(current_date - input_date / (1000 * 60 * 60 * 24)) > 7){
+            frappe.msgprint(__('Back Date More than 7 Days is Not Allowed'))
+            frm.set_value('date',current_date)
+        }
         if(input_date > current_date){
             frappe.msgprint(__('Future Date is Not Allowed.'));
             frm.set_value('date',current_date)
