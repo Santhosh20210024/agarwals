bff9aeb6e2f9823b2d7d3f303c1b7830d35aa210
Balamurugan1207 <bramachandran@techfinite.com>
2024-09-26 12:07:30 +0530

    created report skeleton and getting the configuration from the report configuration

--- agarwals/public/js/report_skeleton.js
+++ agarwals/public/js/report_skeleton.js
@@ -11,16 +11,19 @@ function build_report(report_name){
                 frappe.query_reports[report_name] = {
                     onload: function (report) {
                         report.page.add_inner_button("Apply Filter", function () {
-                            let apply_filters_parameters = JSON.parse(response.message.apply_filter_parameter)
-                            for (let key in apply_filters_parameters){
-                                report.set_filter_value(key, apply_filters_parameters[key])
-                            }
+                            if(response.message.apply_filter_parameter && response.message.apply_filter_parameter != '' && response.message.apply_filter_parameter != null){
+                                let apply_filters_parameters = JSON.parse(response.message.apply_filter_parameter)
+                                for (let key in apply_filters_parameters){
+                                    report.set_filter_value(key, apply_filters_parameters[key])
+                            }}
                         });
                         report.page.add_inner_button("Remove Filter", function () {
+                            if(response.message.remove_filter_parameter && response.message.remove_filter_parameter != '' && response.message.remove_filter_parameter != null){
                             let remove_filters_parameters = JSON.parse(response.message.remove_filter_parameter)
                             console.log(typeof remove_filters_parameters)
                             for (let key in remove_filters_parameters){
                                 report.set_filter_value(key, remove_filters_parameters[key])
+                            }
                             }
                         })
                     },
