bcfc7545f68b830e894729f06d7b34523ce2689a
prem-techfinite <premkumar.g@techfinite.com>
2024-10-04 19:37:27 +0530

    removed

--- agarwals/agarwals/doctype/data_loading_configuration/data_loading_configuration.py
+++ agarwals/agarwals/doctype/data_loading_configuration/data_loading_configuration.py
@@ -1,64 +1,6 @@
 # Copyright (c) 2024, Agarwals and contributors
 # For license information, please see license.txt
-import ast
-import frappe
-import pandas as pd
-import os
+
 from frappe.model.document import Document
-from agarwals.utils.error_handler import log_error
-from agarwals.utils.file_util import (
-    construct_file_url,SITE_PATH,SHELL_PATH,PROJECT_FOLDER)
-
 class DataLoadingConfiguration(Document):
-    
-    
-    def validate(self):
-        doc_val = frappe.get_all("File", {"file_name": self.file_name},['*'])
-        validation_columns = self.get_config_file(self.columns_for_validation)
-        if doc_val :
-            if not self.check_columns():
-                self.change_file(validation_columns)
-        else:
-            self.create_new_file(validation_columns)
-            
-
-    def get_config_file(self,columns_for_validation):
-        try:
-            columns = ast.literal_eval(columns_for_validation)  # Convert the String to lilst using eval
-            return columns
-        except Exception as e:
-            log_error("Error parsing columns for validation: {e}","Data Loading Configuration")
-        
-
-    def check_columns(self):
-        previous_value = frappe.db.get_value("Data Loading Configuration",self.name,'columns_for_validation')
-        return ast.literal_eval(previous_value) == ast.literal_eval(self.columns_for_validation)
-           
-
-    def change_file(self,validation_columns):
-        doc = frappe.get_doc("File",{'file_name':self.file_name})
-        frappe.delete_doc('File',doc.name)   
-        self.create_new_file(validation_columns)
-
-    def create_new_file(self, validation_columns):
-        validation_columns = [col for col in validation_columns if col]
-        directory_path =construct_file_url(SITE_PATH,SHELL_PATH,PROJECT_FOLDER,"/File upload")
-        # Create the directory if it doesn't exist
-        os.makedirs(directory_path, exist_ok=True)
-        if not self.file_name.endswith(('.csv','.xlsx')) :
-            self.file_name = self.file_name + ".csv"
-        site_path = os.path.join(directory_path, self.file_name)
-        df = pd.DataFrame( columns=validation_columns)
-        df.to_csv(site_path, index=False, header=True, columns=validation_columns)        
-
-        with open(site_path, 'rb') as f:
-            file_data = f.read()
-
-        new_file = frappe.get_doc({
-            'doctype': 'File',
-            'file_name': self.file_name,# Extract filename from the path
-            'content': file_data,
-            'is_private': 1  # Change to 1 for private files
-        })
-        new_file.save()
-        frappe.db.commit()
+    pass
