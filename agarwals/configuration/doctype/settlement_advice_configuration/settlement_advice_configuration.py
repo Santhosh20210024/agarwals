a4dc6acf5a9844bae0eba08583c1e638286e4e95
prem-techfinite <premkumar.g@techfinite.com>
2024-10-04 19:38:20 +0530

    updated

--- agarwals/configuration/doctype/settlement_advice_configuration/settlement_advice_configuration.py
+++ agarwals/configuration/doctype/settlement_advice_configuration/settlement_advice_configuration.py
@@ -1,13 +1,7 @@
 # Copyright (c) 2024, Agarwals and contributors
 # For license information, please see license.txt
 
-import frappe
-import pandas as pd
-import os
-import json
 from frappe.model.document import Document
-from agarwals.utils.file_util import (
-    construct_file_url,SITE_PATH,SHELL_PATH,PROJECT_FOLDER)
 
 class SettlementAdviceConfiguration(Document):
 	pass
