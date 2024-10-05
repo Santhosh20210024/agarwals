ea8a63b0d7d94cc273b82c0ab7f90b9e847757ed
Santhosh20210024 <svallatharasu@techfinite.com>
2024-10-05 18:21:43 +0530

    Added custom exception and database utils

---
+++ agarwals/utils/__init__.py
@@ -0,0 +1,16 @@
+import frappe
+
+class DatabaseUtils:
+    @staticmethod
+    def update_doc(doctype, name, **kwargs):
+        """Update a document in the database."""
+        doc = frappe.get_doc(doctype, name)
+        for key, value in kwargs.items():
+            setattr(doc, key, value)
+        doc.save(ignore_permissions=True)
+
+    @staticmethod
+    def clear_doc(doctype, name):
+        """Cancel and delete a document."""
+        frappe.get_doc(doctype, name).cancel()
+        frappe.delete_doc(doctype, name, ignore_permissions=True)
