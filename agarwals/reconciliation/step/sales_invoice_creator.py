170b619c5483e6972c5dbc7312c4d67800e70b85
Gokul-Annadurai <gokul.a@techfinite.com>
2024-10-16 13:19:56 +0530

    laterality field update

--- agarwals/reconciliation/step/sales_invoice_creator.py
+++ agarwals/reconciliation/step/sales_invoice_creator.py
@@ -207,6 +207,7 @@ class SalesInvoiceCreator:
                                         'branch': bill_record.branch, 'branch_type': bill_record.branch_type,
                                         'cost_center': bill_record.cost_center,
                                         'custom_patient_age' : bill_record.patient_age,
+                                        'custom_laterality' : bill_record.laterality,
                                         'items': [
                                             {'item_code': 'Claim', 'rate': bill_record.claim_amount, 'qty': 1}],
                                         'set_posting_time': 1, 'posting_date': bill_record.bill_date,
